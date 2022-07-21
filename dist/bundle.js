d3f1e7d2f6ea71d7f9355a7d39f19af641bb65fd
Saran B M <saran.b.m.001@gmail.com>
2022-07-21 23:46:08 +0530

    basic file structure

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("I'm working!")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsMkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLWFwcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkknbSB3b3JraW5nIVwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
